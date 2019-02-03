eae6780a89aa0b4773a07e3827325db713f51535
GrigoryCode <grigorycode@gmail.com>
2019-02-03 23:10:37 +0300

    Item callbacks

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, :description, presence: true
+
+  after_initialize { }
+  after_save       { }
+  after_create     { }
+  after_update     { }
+  after_destroy    { }
 end
